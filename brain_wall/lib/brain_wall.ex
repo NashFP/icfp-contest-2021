0750041538b9b8ae7bf16b756883d68b32a8335f
Tim Dunnington <accounts@timshouse.com>
2021-07-11 21:46:44 -0500

    Added escript runner and new solution files

--- brain_wall/lib/brain_wall.ex
+++ brain_wall/lib/brain_wall.ex
@@ -2,4 +2,9 @@ defmodule BrainWall do
   @moduledoc """
   Documentation for `BrainWall`.
   """
+
+  def main(args \\ []) do
+    p = BrainWall.Problem.get(args |> Enum.at(0))
+    _ = BrainWall.Solvers.MarksSolver.solve(p)
+  end
 end
